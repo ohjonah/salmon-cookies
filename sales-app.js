4546fa6223b978e47437a747e5548625c239f53c
ohjonah <ohjonah@gmail.com>
2017-05-16 17:59:05 -0700

    finally appended...but not correct

--- sales-app.js
+++ sales-app.js
@@ -35,21 +35,38 @@ function storeSalesByHour() {
   }
 }
 
-// function storeSalesByHour() {
-//   for (var i = 0; i < locations.length; i++) {
-//     for (var j = 0; j < locations[i].cookiesHourlySales.length; j++) {
-//       totalSumByHour[j] = totalSumByHour[j] + locations[i].cookiesHourlySales[j];
-//     }
-//   }
-// }
-
-var firstAndPike = new Store('1st and Pike', 23, 65, 6.3, 6, 20);
-var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2, 6, 20);
-var seattleCenter = new Store('Seattle Center', 11, 38, 3.7, 6, 20);
-var capHill = new Store('Capitol Hill', 20, 38, 2.3, 6, 20);
-var alki = new Store('Alki', 2, 16, 4.6, 6, 20);
+var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
+var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+var capHill = new Store('Capitol Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
 
 var locations = [firstAndPike, seaTacAirport, seattleCenter, capHill, alki];
+
+// DOM Maniuplation
+
+
+// Store.prototype.addTableHeader = function() {
+//   var tableHeader = document.getElementById('shell-head')
+//   var timeArr = [];
+//
+//   for (var i = 0; i < 14; i++) {
+//     if (i < 5) {
+//       timeArr.push('<td>' + this.hoursOfOperation[i] + ' am: ' + '</td>');
+//     } else if (i > 5) {
+//       timeArr.push('<td>' + this.hoursOfOperation[i] + ' pm: ' + '</td>');
+//     } else if (i === 5) {
+//       timeArr.push('<td>' + this.hoursOfOperation[5] + ' pm: ' + '</td>');
+//     }
+//   }
+//   console.log('our time array:', timeArr);
+// }
+//
+// Store.prototype.addTable = function() {
+//   var table = document.getElementById('shell');
+//
+//
+// }
 
 function launchTheConstructor() {
   for (var i = 0; i < locations.length; i++) {
@@ -57,48 +74,31 @@ function launchTheConstructor() {
     locations[i].salesPerHour();
     locations[i].salesSum();
   }
+  storeSalesByHour();
 }
 
 launchTheConstructor();
-storeSalesByHour();
-totalSumByHour;
+totalSumByHour; // displays array of total sales of all stores by hour
 
 
-//
-// Store.prototype.sales_stats = function() {
-//   var container = document.createElement('table');
-//   var tableHeader = document.createElement('thead');
-//   var tableData = document.createElement('td');
-//
-//   for (var i = 0;, )
-//   container.innerHTML =
-//
-//
-// }
-//
-//
-// function listNameAndUlInDom(locations) {
-//     var container = document.createElement('div');
-//     container.innerHTML = '<h3>' + this.name + '</h3>';
-//     document.body.appendChild(container);
-//
-//     var list = document.createElement('ul');
-//     var listArr = [];
-//
-//     for (var i = 0; i < this.operatingHoursArr.length; i++) {
-//       if (i < 5) {
-//         listArr.push('<li>' + this.operatingHoursArr[i] + ' am: ' + this.projectedCookiesSoldPerDay[i] + ' cookies' + '</li>');
-//       } else if (i > 5) {
-//         listArr.push('<li>' + this.operatingHoursArr[i] + ' pm: ' + this.projectedCookiesSoldPerDay[i] + ' cookies' + '</li>');
-//       } else if (i === 5) {
-//         listArr.push('<li>' + this.operatingHoursArr[5] + ' pm: ' + this.projectedCookiesSoldPerDay[i] + ' cookies' + '</li>');
-//       }
-//     }
-//
-//     listArr.push('<li>' + 'Total: ' + this.projectedSumOfCookiesSoldPerDay + ' cookies' + '</li>');
-//
-//     var fullList = listArr.join('');
-//     list.innerHTML = fullList;
-//     document.body.append(list);
-//   }
-// };
+
+
+var table = document.getElementById('shell');
+
+
+for (var i = 0; i < locations.length; i++) {
+  var data = [];
+  var new_row;
+
+  data.push('<td>' + locations[i].name + '</td>');
+
+  for (var j = 0; j < locations[i].cookiesHourlySales.length; j++) {
+    data.push('<td>' + locations[i].cookiesHourlySales[j] + '</td>');
+  }
+
+  new_row = document.createElement('tr');
+  new_row.innerHTML = data[i];
+  table.appendChild(new_row);
+}
+
+console.log('our data array:', data);
